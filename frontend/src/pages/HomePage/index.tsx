dbeafcc435019e6c1c396a5311a674e676b2db90
Gergely Kiss <gergely.kiss@r34dy.io>
2022-09-14 14:50:34 +0200

    buttons changed

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,19 +1,34 @@
 import { FC } from "react";
 import AdbIcon from "@mui/icons-material/Adb";
 import "./style.scss";
+import { Link } from "react-router-dom";
 
 type Props = {};
 
 const HomePage: FC<Props> = () => {
   return (
     <section className="welcome-section">
-      <h1>
-        Welcome to{" "}
-        <span>
-          Bug-tracker
-          <AdbIcon />
-        </span>
-      </h1>
+      <div className="row welcome-wrapper">
+        <h1>
+          Welcome to{" "}
+          <span>
+            Bug-tracker
+            <AdbIcon />
+          </span>
+        </h1>
+        <div className="row d-flex">
+          <div className="col">
+            <Link to="/login">
+              <button className="primary-btn">Login</button>
+            </Link>
+          </div>
+          <div className="col">
+            <Link to="/sign-up">
+              <button className="secondary-btn">Sign up</button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
