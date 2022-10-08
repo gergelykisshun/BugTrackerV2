271c6652a242fe1f7806eb3c32c6691dfa095a23
Gergely Kiss <gergely.kiss@r34dy.io>
2022-10-08 14:09:57 +0200

    mail microservice

---
+++ mail-service/src/main.ts
@@ -0,0 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:8000'],
+  });
+  await app.listen(8002);
+}
+bootstrap();
